2037897c88601dbdbf3ccce782092f0de97488c3
britley.hoff <britleyhoff@gmail.com>
2026-05-11 20:16:08 -0400

    Updated the macro creation modal and also created two example macros to be used with the file transfer demo.

--- Bridge_server/start.ps1
+++ Bridge_server/start.ps1
@@ -1,5 +1,8 @@
 if (-not (Test-Path "lpars.txt")) {
 	    "# id, name, host/IP, port, tls, type, model" | Out-File -Encoding utf8 lpars.txt
 }
+if not exist macros.json (
+  echo []> macros.json
+)
 docker compose down
 docker compose up -d
---
+++ Bridge_server/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+[ ! -f lpars.txt ]   && echo '# id, name, host/IP, port, tls, type, model' > lpars.txt
+[ ! -f macros.json ] && echo '[]' > macros.json
+docker compose down
+docker compose up -d
